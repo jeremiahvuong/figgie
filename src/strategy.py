087eb526d151e6a0dec7ce583544cd6e1fc731d0
Jeremiah Vuong <43306105+jeremiahvuong@users.noreply.github.com>
2025-06-05 12:44:05 -0700

    noisy cleanup

--- src/strategy.py
+++ src/strategy.py
@@ -48,9 +48,7 @@ class Noisy(Strategy):
             try:
                 await asyncio.sleep(self.interval) # Wait for interval seconds
                 order = await self._create_random_order()
-                # If order is None, we skip that iteration,
-                # such that Noisy has to wait another interval to send an order;
-                # unintentionally acts as a "random" method.
+                # Note: if order is None, Noisy will have to wait another interval to send an order
                 if order: await self._order_queue.put(order)
             except asyncio.CancelledError:
                 break
@@ -66,18 +64,13 @@ class Noisy(Strategy):
         order_book_entry = self._order_book[random_suit.name]
         current_bid = order_book_entry["bid"]["price"]
         current_ask = order_book_entry["ask"]["price"]
-        
-        # Represent empty orderbook values as None for easier comparison
-        if current_bid == -999: current_bid = None
-        if current_ask == 999: current_ask = None
 
         if random_side == "bid":
             min_price, max_price = 1, min(15, self._player.dollars)
             # Don't buy if we're have > 3 cards of the suit
             if self._player.inventory[random_suit] >= 3: return None
 
-            # For bids: must be higher than current bid if not our own bid
-            if current_bid is not None and order_book_entry["bid"]["player"] != self._player:
+            if current_bid != -999 and order_book_entry["bid"]["player"] != self._player:
                 min_price = current_bid + 1 # Can't buy for less than the current bid
                 if min_price > max_price: return None # No valid price possible
 
@@ -86,8 +79,7 @@ class Noisy(Strategy):
             # Can't sell if we have none of the suit
             if self._player.inventory[random_suit] < 1: return None
 
-            # For asks: must be lower than current ask if not our own ask
-            if current_ask is not None and order_book_entry["ask"]["player"] != self._player:
+            if current_ask != 999 and order_book_entry["ask"]["player"] != self._player:
                 max_price = current_ask - 1 # Can't sell for more than the current ask
                 if max_price < min_price: return None # No valid price possible
         
